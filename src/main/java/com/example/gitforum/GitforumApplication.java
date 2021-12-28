14dc17eb22c4288451861cfc0754755cbf667371
dengpengfei <741446708@qq.com>
2021-12-28 10:21:09 +0800

    Springboot

---
+++ src/main/java/com/example/gitforum/GitforumApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitforumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitforum/GitforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitforumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
