c05b8d7e650a8d8c2216867b719f6a7aad3d9558
“hanchuangkudu” <741446708@qq.com>
2022-01-12 15:49:42 +0800

    Github第三方登录

--- src/main/java/com/pink/forum/controller/GithubLoginController.java
+++ src/main/java/com/pink/forum/controller/GithubLoginController.java
@@ -7,6 +7,11 @@ import com.pink.forum.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,9 +19,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -25,9 +28,9 @@ import java.util.Map;
 /**
  * @author dengpengfei
  */
+
 @SuppressWarnings("unchecked")
 @Controller
-@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Api(tags = "Github登录")
 public class GithubLoginController {
@@ -36,20 +39,17 @@ public class GithubLoginController {
 
     final UserService userService;
 
-    @GetMapping("github.html")
-    public String index(){
-        return "github";
-    }
-
     @ApiOperation("创建github用户")
-    @GetMapping("main.html")
-    public Result authorizationCode(@RequestParam("code")String code, @RequestParam("state") String state) throws JsonProcessingException {
+    @GetMapping("/success.html")
+    public String authorizationCode(@RequestParam("code")String code, @RequestParam("state") String state) throws JsonProcessingException {
+        Subject subject = SecurityUtils.getSubject();
+
         Map<String, String> map = new HashMap<>();
         map.put("client_id", "da8c28bcfd991b5b2774");
         map.put("client_secret", "bb5e4ba3f6737e60d37e76c4a5d59841697ba7c7");
         map.put("state", state);
         map.put("code", code);
-        map.put("redirect_uri", "http://localhost:8080/api/v1/main.html");
+        map.put("redirect_uri", "http://localhost:8080/success.html");
         Map<String,String> resp = restTemplate.postForObject("https://github.com/login/oauth/access_token", map, Map.class);
         System.out.println(resp);
         HttpHeaders httpheaders = new HttpHeaders();
@@ -57,19 +57,30 @@ public class GithubLoginController {
         httpheaders.add("Authorization", "token " + resp.get("access_token"));
         HttpEntity<?> httpEntity = new HttpEntity<>(httpheaders);
         ResponseEntity<Map> exchange = restTemplate.exchange("https://api.github.com/user", HttpMethod.GET, httpEntity, Map.class);
+
         String username = new ObjectMapper().writeValueAsString(exchange.getBody().get("login"));
         String userId = new ObjectMapper().writeValueAsString(exchange.getBody().get("node_id"));
         String userAvatar = new ObjectMapper().writeValueAsString(exchange.getBody().get("avatar_url"));
 
-        System.out.println("exchange.getBody() = " + new ObjectMapper().writeValueAsString(exchange.getBody()  ));
-        System.out.println("exchange.getBody() = " + username.substring(1, username.length()-1));
-        System.out.println("exchange.getBody() = " + userId.substring(1, userId.length()-1));
-        System.out.println("exchange.getBody() = " + userAvatar.substring(1, userAvatar.length()-1));
+        username = username.substring(1, username.length()-1);
+        userId = userId.substring(1, userId.length()-1);
+        userAvatar = userAvatar.substring(1, userAvatar.length()-1);
 
-        if (userService.githubRegister(username.substring(1, username.length()-1), userId.substring(1, userId.length()-1), userAvatar.substring(1, userAvatar.length()-1)) == 1) {
-            return Result.ok();
-        } else {
-            return Result.bad("登录失败");
+        System.out.println("exchange.getBody() = " + new ObjectMapper().writeValueAsString(exchange.getBody()  ));
+        System.out.println("exchange.getBody() = " + username);
+        System.out.println("exchange.getBody() = " + userId);
+        System.out.println("exchange.getBody() = " + userAvatar);
+
+        if (userService.githubRegister(username.substring(1, username.length()-1), userId.substring(1, userId.length()-1), userAvatar.substring(1, userAvatar.length()-1)) != 1) {
+            return "fail";
+        }
+
+        try {
+            UsernamePasswordToken token = new UsernamePasswordToken(username, "123456");
+            subject.login(token);
+            return "success";
+        } catch (UnknownAccountException | IncorrectCredentialsException e) {
+            return "fail";
         }
     }
 }
